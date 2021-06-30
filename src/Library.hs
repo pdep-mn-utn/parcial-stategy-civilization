7659a4a077b9a9c3af2aab0d726684f33e1626b5
Juan José Contardo <juan.contardo@gmail.com>
2021-06-30 15:58:31 -0300

    Testing

--- src/Library.hs
+++ src/Library.hs
@@ -26,27 +26,51 @@ type SistemaDeDefensa = Ciudad -> Ciudad
 
 
 -- Puno 1
+-- Dada una ciudad necesitamos saber cuáles son los tipos de unidades grosas. Una unidad es grosa si tiene más de 160 potencios de nivel de ataque.
 unidadesGrosas:: Ciudad -> [String]
-unidadesGrosas = map tipo.filter ((>160).nivelDeAtaque).unidades
+unidadesGrosas = map tipo.filter esUnidadGrosa.unidades
 
+esUnidadGrosa:: Unidad ->Bool
+esUnidadGrosa = tieneMasNivelDeAtaqueQue 160
+
+tieneMasNivelDeAtaqueQue:: Potencio -> Unidad ->Bool
+tieneMasNivelDeAtaqueQue potencios = (>potencios).nivelDeAtaque
+
+-- Dada una ciudad queremos saber si tiene un ataque poderoso. Esto ocurre si las primeras 3 unidades de la ciudad tienen más de 100 potencios de nivel de ataque.
 ataquePoderoso :: Ciudad -> Bool
-ataquePoderoso = all((>100).nivelDeAtaque). take 3.unidades
+ataquePoderoso = all(tieneMasNivelDeAtaqueQue 100). take 3.unidades
 
+-- Dada una unidad queremos saber el nivel total que le aportan las herramientas. Esto es la sumatoria de nivel de potencios que aporta cada una.
 poderTotalDeHerramientas :: Unidad -> Potencio 
 poderTotalDeHerramientas = sum.map poder. herramientas 
 
 -- Punto 2
 
+-- Ahora necesitamos determinar el valor del poderOfensivo de una unidad. 
+-- Esto se da con un cálculo que se define en base a una serie de condiciones. 
+
+-- Si tiene más de 5 herramientas el poder ofensivo es 100 potencios sumado al nivel de ataque propio 
+-- más el nivel que aporta cada herramienta. 
+
+-- De lo contrario, si es del tipo "caballería" el cálculo es el doble de la sumatoria del nivel de ataque propio 
+-- y el nivel que aporta cada herramienta. 
+
+-- En caso contrario solamente es el nivel propio de ataque más el nivel que aporta cada herramienta.
+
 poderOfensivo:: Unidad -> Potencio 
 poderOfensivo guerrero 
  | ((>5).length.herramientas) guerrero = 100 + ataqueTotal guerrero 
- | ((=="caballeria").tipo) guerrero = ataqueTotal guerrero * 2
+ | ((=="Caballeria").tipo) guerrero = ataqueTotal guerrero * 2
  | otherwise = ataqueTotal guerrero
 
 ataqueTotal :: Unidad -> Potencio
 ataqueTotal guerrero = nivelDeAtaque guerrero + poderTotalDeHerramientas guerrero 
 
 -- Punto 3
+
+-- Queremos saber si un batallón sobrevive al ataque de otro. Esto ocurre cuando el batallón rival se queda sin unidades. 
+-- Si por el contrario el batallón propio es el que se queda sin unidades, entonces no se cumple la condición. 
+-- Los batallones se deben ir enfrentando en batallas individuales a las unidades en el orden que se encuentran. 
 type Batallon = [Unidad]
 
 sobreviveElBatallon :: Batallon -> Batallon -> Bool
@@ -57,17 +81,24 @@ sobreviveElBatallon (defensor:defensores) (atacante:atacantes)
  | otherwise = sobreviveElBatallon defensores (atacante:atacantes)
 
 -- Punto 4
+
 aumentarnivelDeDefensa :: Defensio -> SistemaDeDefensa
 aumentarnivelDeDefensa cantidad ciudad = ciudad {nivelDeDefensa = nivelDeDefensa ciudad + cantidad}
 
-muralla :: Defensio -> SistemaDeDefensa
+-- La muralla que dada la altura de la misma aumenta el nivel defensivo de la ciudad en el triple de su altura y 
+-- le agrega el prefijo "La gran ciudad de" al nombre de la ciudad. 
+-- Ejemplo: A Beijing que tiene 20 "defensios" de nivel defensivo le agregamos una muralla de 4 metros, esto incrementa 12 "defensios" su nivel
+-- , de manera que pasa a tener 32 "defensios".
+muralla :: Number -> SistemaDeDefensa
 muralla altura ciudad = aumentarnivelDeDefensa (altura * 3) ciudad {
   nombre = "La gran " ++ nombre ciudad
 }
-
+-- Las torres de vigilancia que nos permiten preparar a la ciudad ante un posible ataque con antelación. Esto incrementa el nivel de defensa en 40 "defensios".
 torresDeVigilancia:: SistemaDeDefensa
 torresDeVigilancia = aumentarnivelDeDefensa 40
 
+-- El centro de entrenamiento que incrementa un determinado valor el nivel de ataque propio que tiene cada unidad que posee su batallón de defensa. 
+-- Además sube 10 "defensios" el nivel de defensa de la ciudad.
 centroEntrenamiento :: Potencio -> SistemaDeDefensa
 centroEntrenamiento incremento ciudad = aumentarnivelDeDefensa 10 ciudad{
   unidades = map (incrementarPoderAtaque incremento).unidades $ ciudad
@@ -76,9 +107,14 @@ centroEntrenamiento incremento ciudad = aumentarnivelDeDefensa 10 ciudad{
 incrementarPoderAtaque:: Potencio -> Unidad -> Unidad
 incrementarPoderAtaque incremento unidad = unidad {nivelDeAtaque = nivelDeAtaque unidad + incremento}
 
+-- Instalar bancos en las plazas. Esto no incrementa el nivel de defensa bajo ningún punto de vista.
+instalarBancos :: SistemaDeDefensa
+instalarBancos = id
 
 -- Punto 5
-
+-- odelar la función poderDefensivo que nos da el valor del nivel de defensa luego de activar todos los sistemas de defensa en la ciudad. 
+-- Dar un ejemplo de cómo se invocará a la ciudad Persepolis con un nivel de defensa de 10, sin batallón de defensa pero con una muralla de 5 metros, 
+-- un centro de entrenamiento de 15 "defensios" adicionales para las unidades del batallón y una torre de vigilancia.
 poderDefensivo :: Ciudad -> Defensio 
 poderDefensivo ciudad = nivelDeDefensa.foldr ($) ciudad $ sistemasDeDefensa ciudad 
 
@@ -87,6 +123,8 @@ poderResultante = poderDefensivo $ Ciudad "Persepolis" 10 [] [muralla 5, centroE
 
 
 -- Punto 6
+-- Saber si una ciudad sobrevive al ataque de un batallón. Esto ocurre con dos condiciones: el batallón de defensa sobrevive 
+-- al ataque del batallón atacante o bien el poder defensivo de la ciudad es superior al poder total de ataque del batallón.
 sobreviveCiudad :: Ciudad -> Batallon -> Bool 
 sobreviveCiudad ciudad batallon = sobreviveElBatallon (unidades ciudad) batallon || poderDefensivo ciudad > poderAtaqueBatallon batallon
 
--- src/Spec.hs
+++ src/Spec.hs
@@ -1,11 +1,81 @@
 module Spec where
-import PdePreludat
 import Library
+import PdePreludat
 import Test.Hspec
+
+unidadTanque = Unidad "Tanque" 105 [("Tanqueta",20)] 
+unidad = Unidad "Caballeria" 101 [("fusil",10),("pistola",20),("cuchillo",20),("remo",20),("chancleta",20),("ametralladora",20)] 
+unidadGrosa = Unidad "Grosa" 200 [] 
+moto = Unidad "Moto" 50 []
+caballeria = Unidad "Caballeria" 200 [] 
+
+batallonPersepolis = [caballeria, moto]
+batallonUr = [unidad,unidadTanque,unidadGrosa]
+
+persepolis = Ciudad "Persepolis" 100 [unidad, unidadGrosa, unidadTanque] []
+ur = Ciudad "Ur" 1000 [unidad, moto, unidadGrosa, unidadTanque] []
+beijing = Ciudad "Beijing" 20 [unidad] [muralla 4,torresDeVigilancia]
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "Punto 1" $ do
+    it "Dada una unidad con suficiente poder de ataque, entonces es grosa" $ do
+      unidadGrosa `shouldSatisfy` esUnidadGrosa
+    it "Dada una unidad sin suficiente poder de ataque, entonces no es grosa" $ do
+      unidad `shouldNotSatisfy` esUnidadGrosa
+    it "Dada una ciudad, se pueden obtener sus unidades grosas" $ do
+      unidadesGrosas persepolis `shouldContain` ["Grosa"]
+      (length.unidadesGrosas) persepolis `shouldBe` 1
+    it "Dada una ciudad con sus primeras n unidades con suficiente poder, entonces tiene ataque poderoso" $ do
+      persepolis `shouldSatisfy` ataquePoderoso
+    it "Dada una ciudad con sus primeras n unidades con suficiente poder, entonces tiene ataque poderoso" $ do
+      ur `shouldNotSatisfy` ataquePoderoso
+    it "Dada una unidad con herramientas, entonces se obtiene el poder total de sus herramientas" $ do
+      poderTotalDeHerramientas unidadTanque `shouldBe` 20
+    it "Dada una unidad sin herramientas, entonces el poder total de sus herramientas es cero" $ do
+      poderTotalDeHerramientas moto `shouldBe` 0
 
+  describe "Punto 2" $ do 
+    it "Dada una unidad, entoces su poder de ataque total se calcula como el propio más el de sus herramientas" $ do
+      ataqueTotal unidadTanque `shouldBe` 125
+    it "Dada una unidad con suficientes herramientas, entonces su poder ofensivo es el poder base + su ataque total" $ do 
+      poderOfensivo unidad `shouldBe` 311
+    it "Dada una unidad sin suficientes herramientas que es de un tipo específico, entonces su poder ofensivo es el ataque total por un coeficiente" $ do 
+      poderOfensivo caballeria `shouldBe` 400
+    it "Dada una unidad sin suficientes herramientas ni del tipo específico, entonces su poder ofensivo es su poder de ataque total" $ do
+      poderOfensivo moto `shouldBe` 50
+
+  describe "Punto 3" $ do 
+    it "Dada dos batallones, sobrevive el primero si tiene el poder ofensivo suficiente para vencer al otro" $ do
+      batallonUr `shouldSatisfy` sobreviveElBatallon batallonPersepolis
+    it "Dada dos batallones, no sobrevive el primero si no cuenta con el poder ofensivo suficiente para vencer al otro" $ do
+      batallonPersepolis `shouldNotSatisfy` sobreviveElBatallon batallonUr
+
+  describe "Punto 4" $ do 
+    it "Dada un nivel de defensios y una ciudad, entonces aumentarNivelDeDefensa le agrega dicho valor" $ do
+      (nivelDeDefensa.aumentarnivelDeDefensa 10) persepolis `shouldBe` 110
+    it "Dada una muralla con una cierta altura y una ciudad, entonces muralla le agrega dicho valor por un factor y le antepone un prefijo al nombre" $ do
+      (nivelDeDefensa.muralla 4) beijing `shouldBe` 32
+      (nombre.muralla 4) beijing `shouldBe` "La gran Beijing"
+    it "Dada una torre de vigilacia y una ciudad, entonces aumenta su nivel de defensios en un valor fijo" $ do
+      (nivelDeDefensa.torresDeVigilancia) beijing `shouldBe` 60
+    it "Dado un centro de entrenamiento y una ciudad, entonces aumenta su nivel de defensios en un valor fijo" $ do
+      (nivelDeDefensa.centroEntrenamiento 20) beijing `shouldBe` 30
+    it "Dado un centro de entrenamiento y una ciudad, entonces aumenta el nivel de cada integrante del batallón de potencios en un valor determinado" $ do
+      (nivelDeAtaque.head.unidades.centroEntrenamiento 10) beijing `shouldBe` 111
+  
+  describe "Punto 5" $ do 
+    it "Dada una ciudad con sistemas de defensa, entonces el poder defensivo es su nivel de defensa luego de plaicar los sistemas en la misma" $ do
+      poderDefensivo beijing `shouldBe` 72
+    it "Dada una ciudad sin sistemas de defensa, entonces el poder defensivo es su nivel de defensa" $ do
+      poderDefensivo persepolis `shouldBe` 100
+  
+  describe "Punto 6" $ do
+    it "Dado un batallón, poderAtaqueBatallon obtiene el total del mismo" $ do
+      poderAtaqueBatallon batallonUr `shouldBe` 636
+    it "Dada una ciudad con un battallón de suficiente energía para vencer al atacante, entonces sobrevive la ciudad" $ do
+      batallonUr `shouldSatisfy` sobreviveCiudad persepolis {unidades = batallonPersepolis}
+    it "Dada una ciudad con un battallón sin suficiente energía para vencer al atacante perco con suficiente poder de defensa, entonces sobrevive la ciudad" $ do
+      batallonPersepolis `shouldSatisfy` sobreviveCiudad ur 
+    it "Dada una ciudad con un battallón sin suficiente energía para vencer al atacante ni suficiente poder de defensa, entonces no sobrevive la ciudad" $ do
+      batallonUr `shouldNotSatisfy` sobreviveCiudad persepolis 
